40d81b4f34099c18f0467ef2fa91a25a608d2360
johan7004 <johanstephen7004@gmail.com>
2022-03-27 18:27:21 +0300

    calculator with useReducer

--- calculator/src/components/calcDisplay/CalcDisplay.js
+++ calculator/src/components/calcDisplay/CalcDisplay.js
@@ -4,10 +4,10 @@ import './calcDisplay.css'
 export default function CalcDisplay(props) {
   return (
     <div className="calcDisplay__container">
-    <form>
-    <input className='calcDisplay__output' type="text" defaultValue={props.displayValue}  value={props.displayValue}/>
-    <input className='calcDisplay__output--result' type="text" defaultValue={props.displayValue} value={props.result}/>
-    </form>
+    
+    <input className='calcDisplay__output' type="text" placeholder={props.displayValue}></input>
+    <input className='calcDisplay__output' type="text" placeholder={`=>  ${isNaN(props.displayResult)?`Syntax Error`:props.displayResult}`}></input>
+    
     
     </div>
   )
--- calculator/src/components/calcValue/CalcValue.js
+++ calculator/src/components/calcValue/CalcValue.js
@@ -1,86 +1,115 @@
-import {React, useEffect, useState} from 'react';
-import CalcDisplay from '../calcDisplay/CalcDisplay';
-import './calcValue.css'
+import { React, useState, useReducer } from "react";
+import CalcDisplay from "../calcDisplay/CalcDisplay";
+import "./calcValue.css";
 
+let initialState = 0;
+
+function reducer(state, action) {
+  let operandValues = null;
+  if (/[+-/*]$/.test(action.payload)) {
+    console.log(`pass `);
+    operandValues = action.payload * 0;
+  } else {
+    console.log(`error`);
+
+    operandValues = action.payload;
+  }
+
+  switch (action.type) {
+    case "CALC":
+      // eslint-disable-next-line no-eval
+      return eval(operandValues);
+
+    case "RESET":
+      return initialState;
+    default:
+      return state;
+  }
+}
 export default function CalcValue() {
+  const [operands, setOperands] = useState(" ");
+  const [value, dispatch] = useReducer(reducer, initialState);
 
-  const [value, setValue]=useState("");
-  const [evalValue, setEvalValue]= useState("");
-
-
-
-    const numGenerator =()=>{
-
-        let numbers=[];
-
-        for(let i=1; i<10; i++){
-
-            
-                numbers.push(<button key={i} className="calcButtons__numbers-digits" onClick={calcValue}>{i}</button>)
-            
-
-        }
-        
-
-        return numbers;
-
+  function changeValue(e) {
+    e.preventDefault();
+    const pressedNum = e.target.innerText;
+    setOperands((operands) => operands + pressedNum);
+    if (pressedNum === "CE") {
+      setOperands("");
     }
+  }
 
-    const calcValue =(e)=>{
+  let numbers = [];
 
-        let numberPressed = e.target.innerHTML;
-        
-    
-          setValue((prevValue) => prevValue + numberPressed );
-        
-        
-    }
-
-  
-
-    useEffect(()=>{
-
-      if(value.endsWith('+')||value.endsWith('-')||value.endsWith('/')||value.endsWith('*')){
-        console.log(`syntax Error`)
-      }else{
-        console.log(eval(value))
-        setEvalValue(eval(value))
-      }
-    })
-
-    const calcEqual=()=>{
-      console.log(evalValue)
-      setValue(`${evalValue}`)
-    }
-    const resetCalc=()=>{
-      setValue("")
-    }
-
-
-    
+  for (let i = 1; i < 10; i++) {
+    numbers.push(i);
+  }
 
   return (
-      <div>
-      <CalcDisplay
-      displayValue={value}
-      result={evalValue}
-      />
-    <div className="calcButtons__container">
-    <div className="calcButtons__numbers">
-    <button className="calcButtons__numbers-digits" onClick={calcValue}>0</button>
-    {numGenerator()}
-
-    
+    <div>
+      <CalcDisplay displayValue={operands} displayResult={value} />
+      <div className="calcButtons__container">
+        <div className="calcButtons__numbers">
+          <button
+            className="calcButtons__numbers-digits"
+            onClick={(e) => changeValue(e)}
+          >
+            0
+          </button>
+          {numbers.map((number, i) => {
+            return (
+              <button
+                key={i}
+                className="calcButtons__numbers-digits"
+                onClick={changeValue}
+              >
+                {number}
+              </button>
+            );
+          })}
+        </div>
+        <div className="calcButtons__operation">
+          <button
+            className="calcButtons__operation-operators"
+            onClick={(e) => changeValue(e)}
+          >
+            +
+          </button>
+          <button
+            className="calcButtons__operation-operators"
+            onClick={(e) => changeValue(e)}
+          >
+            -
+          </button>
+          <button
+            className="calcButtons__operation-operators"
+            onClick={(e) => changeValue(e)}
+          >
+            *
+          </button>
+          <button
+            className="calcButtons__operation-operators"
+            onClick={(e) => changeValue(e)}
+          >
+            /
+          </button>
+          <button
+            className="calcButtons__operation-operators"
+            onClick={() => dispatch({ type: "CALC", payload: operands })}
+          >
+            =
+          </button>
+          <button
+            className="calcButtons__operation-operators"
+            onClick={(e) => {
+              changeValue(e);
+              dispatch({ type: "RESET", payload: operands });
+            }}
+          >
+            CE
+          </button>
+        </div>
+      </div>
     </div>
-    <div className="calcButtons__operation">
-    <button className="calcButtons__operation-operators" onClick={calcValue}>+</button>
-    <button className="calcButtons__operation-operators" onClick={calcValue}>-</button>
-    <button className="calcButtons__operation-operators" onClick={calcValue}>*</button>
-    <button className="calcButtons__operation-operators" onClick={calcValue}>/</button>
-    <button className="calcButtons__operation-operators" onClick={calcEqual}>=</button>
-    <button className="calcButtons__operation-operators" onClick={resetCalc}>CE</button>
-    </div>
-    </div>
-    </div>
-  )
+  );
 }
